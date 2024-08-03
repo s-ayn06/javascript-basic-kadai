40fa889138e7ccab7c783d30dea9a06db3c38235
s-ayn06 <shimizu.ayn69@outlook.jp>
2024-08-03 16:17:22 +0900

    kadai_020
    kadai_020

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+const addBtn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+addBtn.addEventListener('click', () => {
+  setTimeout(() => {
+    text.textContent = "ボタンをクリックしました";
+   });})
